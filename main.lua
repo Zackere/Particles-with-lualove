eaecda8a1dba687d4c6f5811efd58b410821b83a
zackere <wo1001@wp.pl>
2019-02-08 23:02:15 +0100

    Basic functionality: keypresses etc

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
+Exponent = 1 / 27
+Factor = 8
+
 function CalculateForce(xpos, ypos, xcenter, ycenter)
 	local rx = xcenter - xpos
 	local ry = ycenter - ypos
 	local mod = rx^2 + ry^2
 	local arg = 2 * math.atan(ry / (math.sqrt(mod) + rx))
-	mod = math.pow(mod, 0.037037) * 8
+	mod = math.pow(mod, Exponent) * Factor
 	return mod * math.cos(arg), mod * math.sin(arg)
 end
 
@@ -14,7 +17,7 @@ function love.load()
 	PartArray = {}
 	local ScreenWidth = love.graphics.getWidth()
 	local ScreenHeight = love.graphics.getHeight()
-	Nparticles = 4000
+	local Nparticles = 4000
 	for i=1,Nparticles do
 		PartArray[i] = Particle:new{xpos = ScreenWidth * math.random(), ypos = ScreenHeight * math.random(),
 	xvel = 0, yvel = 0}
@@ -24,6 +27,16 @@ end
 function love.keypressed(key)
 	if key == "escape" then
 		love.event.quit()
+	elseif key == "w" then
+		Exponent = Exponent + 1e-2
+	elseif key == "s" then
+		Exponent = math.max(Exponent - 1e-2, 0)
+	elseif key == "e" then
+		Factor = Factor + 1
+	elseif key == "d" then
+		Factor = math.max(Factor - 1, 0)
+	elseif key == "r" then
+		love.load()
 	end
 end
 
@@ -45,9 +58,9 @@ end
 
 function love.update(dt)
 	local x,y = love.mouse.getPosition()
-	for i=1,Nparticles do
+	for i,v in ipairs(PartArray) do
 		local xfor, yfor = CalculateForce(PartArray[i].xpos, PartArray[i].ypos, x, y)
-		PartArray[i]:move(xfor, yfor, dt)
+		v:move(xfor, yfor, dt)
 	end
 
 end
